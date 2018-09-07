da00c6f154975523cf5921513e1f4f67e805ef6e
Sergey12345 <1cpromo@mail.ru>
2018-09-07 14:38:57 +0300

    123

--- src/telran/time/WorkingDays.java
+++ src/telran/time/WorkingDays.java
@@ -5,26 +5,41 @@ import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
-import java.util.Arrays;
 
 public class WorkingDays implements TemporalAdjuster {
 
-	DayOfWeek[] dayOfWeeks;
-	int i;
+	DayOfWeek[] weekend;
+	int 		i;
+	int 		numberOfWorkingDaysAWeek;
 	
-	public WorkingDays(int i, DayOfWeek[] dayOfWeeks) {
-		this.i 			= i;
-		this.dayOfWeeks = dayOfWeeks;
+	public WorkingDays(int i, DayOfWeek[] weekend) {
+		this.i 							= i;
+		this.weekend 					= weekend;
+		this.numberOfWorkingDaysAWeek	= 7 - weekend.length;
 	}
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		for (int j = 0; j < i; j++) {
+		int numberOfWeeks 	= i / numberOfWorkingDaysAWeek;
+		int numberOfDays	= i % numberOfWorkingDaysAWeek;
+		temporal 			= temporal.plus(numberOfWeeks, ChronoUnit.WEEKS);
+		
+		int j = 0;
+		while(j < numberOfDays) {
 			temporal = temporal.plus(1, ChronoUnit.DAYS);
-			if (Arrays.binarySearch(dayOfWeeks, DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK))) > -1) {
-				j--;
+			if (!findDayInArray(weekend, DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK)))) {
+				j++;
 			}
 		}
 		return temporal;
+	}
+
+	private boolean findDayInArray(DayOfWeek[] a, DayOfWeek key) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].equals(key)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
